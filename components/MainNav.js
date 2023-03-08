8c47ec954bd268f92bed4336a2276822a8f3f87d
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-08 13:57:42 -0500

    update

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,14 +1,20 @@
-import { Navbar } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function MainNav(){
     return (
         <>
-            <Navbar className="fixed-top navbar-dark" bg="primary">
+            <Navbar className="fixed-top" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand>Tom Bragagnolo</Navbar.Brand>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/search">Advanced Search</Nav.Link>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/search">Advanced Search</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
