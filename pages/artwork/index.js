eb7118846856c71448b9837d844e1a23a14be425
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 18:26:35 -0500

    update

--- pages/artwork/index.js
+++ pages/artwork/index.js
@@ -10,4 +10,16 @@ export default function Artwork(){
     let finalQuery = router.asPath.split('?')[1];
 
     const {data, error} = useSWR(`https://collectionapi.metmuseum.org/public/collection/v1/search?${finalQuery}`);
+
+    function previous(){
+        if(page > 1){
+            setPage(prev => prev - 1);
+        }
+    }
+
+    function next(){
+        if(page < artworkList.length){
+            setPage(prev => prev + 1);
+        }
+    }
 }
