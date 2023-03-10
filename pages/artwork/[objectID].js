07bf4e30087af23a88a9ae10ebf98fa4ec7c2d32
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 17:39:21 -0500

    update

--- pages/artwork/[objectID].js
+++ pages/artwork/[objectID].js
@@ -2,5 +2,8 @@ import { useRouter } from "next/router";
 import ArtworkCardDetail from "@/components/ArtworkCardDetail";
 
 export default function ArtworkByld(){
+    const router = useRouter();
+    const {objectID} = router.query
+
     
 }
