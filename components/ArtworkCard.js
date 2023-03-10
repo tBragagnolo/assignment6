fcb012df0a9cddddec22ccefa5c3d56f9d6a2d05
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 16:11:51 -0500

    update

--- components/ArtworkCard.js
+++ components/ArtworkCard.js
@@ -5,6 +5,7 @@ import Card from 'react-bootstrap/Card';
 
 export default function ArtworkCard(props){
     const {data, error} = useSWR();
+    //374686
 
     if(error){ return <Error statusCode={404} /> }
 
@@ -14,7 +15,9 @@ export default function ArtworkCard(props){
                 <Card>
                     {data?.primaryImageSmall ? <Card.Img src={data?.primaryImageSmall}/> : <Card.Img src="https://via.placeholder.com/375x375.png?text=[+Not+Available+]"/>}
                     {data?.title ? <Card.Title>{data?.title}</Card.Title> : <Card.Title>N/A</Card.Title>}
-
+                    {data?.objectDate ? <Card.Text><strong>Date:</strong> {data?.objectDate}</Card.Text> : <Card.Text><strong>Date:</strong> N/A</Card.Text>}
+                    {data?.classification ? <Card.Text><strong>Classification:</strong> {data?.classification}</Card.Text> : <Card.Text><strong>Classification:</strong> N/A</Card.Text>}
+                    {data?.medium ? <Card.Text><strong>Medium:</strong> {data?.medium}</Card.Text> : <Card.Text><strong>Medium:</strong> N/A</Card.Text>}
                 </Card>
             </>
         )
