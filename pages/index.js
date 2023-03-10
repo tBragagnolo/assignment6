1617796fddd5be63e53eb67aa55005338ea50dbf
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 18:00:37 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,14 @@ export default function Home() {
   return (
     <>
       <Image src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg" fluid={true} rounded={true}/>
-
+      <Row>
+        <Col md={6}>
+        
+        </Col>
+        <Col md={6}>
+          
+        </Col>
+      </Row>
     </>
   )
 }
