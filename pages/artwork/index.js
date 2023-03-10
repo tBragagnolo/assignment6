e1069634c1ec0bc8096194a5913fd0a5dad8d680
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 18:20:35 -0500

    update

--- pages/artwork/index.js
+++ pages/artwork/index.js
@@ -1,7 +1,9 @@
 import userSWR from "swr"
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 export default function Artwork(){
     const PER_PAGE = 12;
-    
+    const [artworkList, setArtworkList] = useState();
+    const [page, setPage] = useState(1);
 }
