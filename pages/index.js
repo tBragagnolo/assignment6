6b2f26024437d6c54732f479223e980d8ab40b22
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 17:59:40 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Image from 'react-bootstrap/Image'
+import { Row, Col } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <>
       <Image src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg" fluid={true} rounded={true}/>
+
     </>
   )
 }
