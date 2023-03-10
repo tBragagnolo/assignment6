5ae006d9b797ea6201d2623ab7f4369e91f9e8c2
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 17:35:22 -0500

    update

--- components/ArtworkCardDetail.js
+++ components/ArtworkCardDetail.js
@@ -5,28 +5,27 @@ import Card from 'react-bootstrap/Card';
 import Link from "next/link";
 import { Container } from "react-bootstrap";
 
-export default function ArtworkCard(props){
+export default function ArtworkCardDetail(props){
     const {data, error} = useSWR(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${props.objectID}`);
-
-    var btnURL = "/artwork/" + props.objectID;
+    //10496
 
     if(error){ return <Error statusCode={404} /> }
 
     if(data){
         return(
             <>
-                <Card style={{ width: '18rem' }}>
-                    {data?.primaryImageSmall ? <Card.Img src={data?.primaryImageSmall}/> : <Card.Img src="https://via.placeholder.com/375x375.png?text=[+Not+Available+]"/>}<br />
+                <Card>
+                    {data?.primaryImage && <Card.Img src={data?.primaryImage}/>}<br />
                     <Container>
                         {data?.title ? <Card.Title>{data?.title}</Card.Title> : <Card.Title>N/A</Card.Title>}
                         <Card.Text>
                             {data?.objectDate ? <span><b>Date:</b> {data?.objectDate}</span> : <span><b>Date:</b> N/A</span>}<br/>
                             {data?.classification ? <span><b>Classification:</b> {data?.classification}</span> : <span><b>Classification:</b> N/A</span>}<br/>
-                            {data?.medium ? <span><b>Medium:</b> {data?.medium}</span> : <span><b>Medium:</b> N/A</span>}
+                            {data?.medium ? <span><b>Medium:</b> {data?.medium}</span> : <span><b>Medium:</b> N/A</span>}<br /><br/>
+                            {data?.artistDisplayName ? <span><b>Artist:</b> {data?.artistDisplayName} (<a href={data?.artistWikidata_URL} target="_blank" rel="noreferrer" >wiki</a>)</span> : <span><b>Artist:</b> N/A</span>}<br/>
+                            {data?.creditLine ? <span><b>Credit Line:</b> {data?.creditLine}</span> : <span><b>Credit Line:</b> N/A</span>}<br/>
+                            {data?.dimensions ? <span><b>Dimensions:</b> {data?.dimensions}</span> : <span><b>Dimensions:</b> N/A</span>}<br/>
                         </Card.Text>
-                        <Link href={btnURL} passHref>
-                            <Button variant="outline-dark"><b>ID:</b> {data?.objectID}</Button>
-                        </Link>
                     </Container><br />
                 </Card>
             </>
