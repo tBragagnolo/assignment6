edc898d0c8af6cd92bbed3474fe5bce6bc41b116
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 16:00:54 -0500

    update

--- components/ArtworkCard.js
+++ components/ArtworkCard.js
@@ -1,9 +1,21 @@
+import useSWR from "swr"
 import Error from "next/error"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export default function ArtworkCard(props){
-    return(
-        <>
-            
-        </>
-    )
+    const {data, error} = useSWR();
+
+    if(error){ return <Error statusCode={404} /> }
+
+    if(data){
+        return(
+            <>
+                <Card>
+                    {data?.primaryImageSmall ? <Card.Img src={data?.primaryImageSmall}/> : <Card.Img src="https://via.placeholder.com/375x375.png?text=[+Not+Available+]"/>}
+                    
+                </Card>
+            </>
+        )
+    }
 }
