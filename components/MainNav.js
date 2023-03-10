1f87af5dbf36368b982b58086907d4897827d829
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 14:18:49 -0500

    update

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap';
 
 export default function MainNav(){
     return (
@@ -14,6 +16,15 @@ export default function MainNav(){
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/search">Advanced Search</Nav.Link>
                         </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
