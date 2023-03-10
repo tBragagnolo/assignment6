2fc46721ffff698186a245476e1b38391dbda9fc
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 17:11:13 -0500

    update

--- components/ArtworkCard.js
+++ components/ArtworkCard.js
@@ -3,8 +3,9 @@ import Error from "next/error"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Link from "next/link";
+import { Container } from "react-bootstrap";
 
-export default function ArtworkCard(props){
+function ArtworkCard(props){
     const {data, error} = useSWR(`https://collectionapi.metmuseum.org/public/collection/v1/objects/811044`);
     //811044
 
@@ -13,15 +14,17 @@ export default function ArtworkCard(props){
     if(data){
         return(
             <>
-                <Card>
-                    {data?.primaryImageSmall ? <Card.Img src={data?.primaryImageSmall}/> : <Card.Img src="https://via.placeholder.com/375x375.png?text=[+Not+Available+]"/>}
-                    {data?.title ? <Card.Title>{data?.title}</Card.Title> : <Card.Title>N/A</Card.Title>}
-                    <Card.Text>
-                        {data?.objectDate ? <span><b>Date:</b> {data?.objectDate}</span> : <span><b>Date:</b> N/A</span>}<br/>
-                        {data?.classification ? <span><b>Classification:</b> {data?.classification}</span> : <span><b>Classification:</b> N/A</span>}<br/>
-                        {data?.medium ? <span><b>Medium:</b> {data?.medium}</span> : <span><b>Medium:</b> N/A</span>}
-                    </Card.Text>
-                    <Button passHref={<Link href="/artwork/"></Link>}>{data?.objectID}</Button>
+                <Card style={{ width: '18rem' }}>
+                    {data?.primaryImageSmall ? <Card.Img src={data?.primaryImageSmall}/> : <Card.Img src="https://via.placeholder.com/375x375.png?text=[+Not+Available+]"/>}<br />
+                    <Container>
+                        {data?.title ? <Card.Title>{data?.title}</Card.Title> : <Card.Title>N/A</Card.Title>}
+                        <Card.Text>
+                            {data?.objectDate ? <span><b>Date:</b> {data?.objectDate}</span> : <span><b>Date:</b> N/A</span>}<br/>
+                            {data?.classification ? <span><b>Classification:</b> {data?.classification}</span> : <span><b>Classification:</b> N/A</span>}<br/>
+                            {data?.medium ? <span><b>Medium:</b> {data?.medium}</span> : <span><b>Medium:</b> N/A</span>}
+                        </Card.Text>
+                        <Button variant="outline-dark" passHref={<Link href="/artwork/"></Link>}><b>ID:</b> {data?.objectID}</Button>
+                    </Container><br />
                 </Card>
             </>
         )
@@ -30,7 +33,7 @@ export default function ArtworkCard(props){
     else{ return null }
 }
 
-function ArtworkCardDetail(){
+export default function ArtworkCardDetail(){
     const {data, error} = useSWR(`https://collectionapi.metmuseum.org/public/collection/v1/objects/811044`);
     //811044
 
