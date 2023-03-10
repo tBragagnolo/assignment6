e25a23e44f14fa102224ea6a76ee5af4d679c9f8
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 16:19:14 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import MainNav from "@/components/MainNav"
+import ArtworkCard from "@/components/ArtworkCard"
 
 export default function Home() {
   return (
     <>
-      <p>Home</p>
+      <ArtworkCard/>
     </>
   )
 }
