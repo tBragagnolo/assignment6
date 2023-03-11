faeaa3643388b907fff0540a897be202d147ebae
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 19:05:44 -0500

    update

--- pages/artwork/index.js
+++ pages/artwork/index.js
@@ -1,6 +1,9 @@
 import useSWR from "swr"
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import Error from "next/error";
+import { Row, Col } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export default function Artwork(){
     const PER_PAGE = 12;
@@ -21,5 +24,38 @@ export default function Artwork(){
         if(page < artworkList.length){
             setPage(prev => prev + 1);
         }
+    }
+
+    useEffect(()=>{
+        if(data){
+            let results;
+
+            for (let i = 0; i < data?.objectIDs?.length; i += PER_PAGE) {
+                const chunk = data?.objectIDs.slice(i, i + PER_PAGE);
+                results.push(chunk);
+            }  
+            
+            setArtworkList(results);
+        }
+        setPage(1);
+    }, [data]);
+
+    if(error){ return <Error statusCode={404} /> }
+
+    if(artworkList){
+        return(
+            <>
+                <Row className="gy-4">
+                    {artworkList.length>0 ? artworkList[page - 1].map(m =>{
+                        <Col lg={3} key={currentObjectID}><ArtworkCard objectID={m.objectID} /></Col>
+                    }): <Card>
+                            <h4>Nothing Here</h4>
+                            <span>Try searching something else</span>
+                        </Card>
+                    }    
+                </Row>
+                
+            </>
+        )
     }
 }
