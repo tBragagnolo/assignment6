58037defb4d8d25d1740ee0fccde0297d534a27a
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-03-10 21:12:59 -0500

    update

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,70 @@
-import ArtworkCard from "@/components/ArtworkCard";
+import { Form } from "react-bootstrap"
+import { Row, Col } from "react-bootstrap"
+import Button from "react-bootstrap"
 
 export default function AdvancedSearch(){
     return(
-        <>
-            <ArtworkCard objectID={10496}/>
+        <> 
+            <Form>
+                <Row>
+                    <Col>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Search Query</Form.Label>
+                        <Form.Control type="text" placeholder="" name="q" />
+                    </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={4}>
+                    <Form.Label>Search By</Form.Label>
+                    <Form.Select name="searchBy" className="mb-3">
+                        <option value="title">Title</option>
+                        <option value="tags">Tags</option>
+                        <option value="artistOrCulture">Artist or Culture</option>
+                    </Form.Select>
+                    </Col>
+                    <Col md={4}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Geo Location</Form.Label>
+                        <Form.Control type="text" placeholder="" name="geoLocation" />
+                        <Form.Text className="text-muted">
+                        Case Sensitive String (ie &quot;Europe&quot;, &quot;France&quot;, &quot;Paris&quot;, &quot;China&quot;, &quot;New York&quot;, etc.), with multiple values separated by the | operator
+                    </Form.Text>
+                    </Form.Group>
+                    </Col>
+                    <Col md={4}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Medium</Form.Label>
+                        <Form.Control type="text" placeholder="" name="medium"/>
+                        <Form.Text className="text-muted">
+                        Case Sensitive String (ie: &quot;Ceramics&quot;, &quot;Furniture&quot;, &quot;Paintings&quot;, &quot;Sculpture&quot;, &quot;Textiles&quot;, etc.), with multiple values separated by the | operator
+                    </Form.Text>
+                    </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <Form.Check
+                        type="checkbox"
+                        label="Highlighted"
+                        name="isHighlight"
+                    />
+                    <Form.Check
+                        type="checkbox"
+                        label="Currently on View"
+                        name="isOnView"
+                    />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <br />
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                    </Col>
+                </Row>
+            </Form> 
         </>
     )
 }
