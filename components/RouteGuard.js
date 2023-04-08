6ef2ad2e7eb9ebdf0ba854f8cafc550e7943e287
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-04-08 18:37:58 -0400

    update

--- components/RouteGuard.js
+++ components/RouteGuard.js
@@ -1,3 +1,27 @@
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+
+const PUBLIC_PATHS = ['/login', '/', '/_error'];
+
 export default function RouteGuard(props) {
-    return <>{props.children}</>
+  const router = useRouter();
+
+  useEffect(() => {
+    authCheck(router.pathname);
+
+    router.events.on('routeChangeComplete', authCheck);
+
+    return () => {
+      router.events.off('routeChangeComplete', authCheck);
+    };
+  }, []);
+
+  function authCheck(url) {
+    const path = url.split('?')[0];
+    if (!PUBLIC_PATHS.includes(path)) {
+      console.log(`trying to request a secure path: ${path}`);
+    }
+  }
+
+  return <>{props.children}</>
 }
