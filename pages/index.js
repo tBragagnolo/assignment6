aae9db8264f1badd604d2273eb4dcc0a6403946d
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-04-08 19:32:12 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,13 @@
 
 
 import Image from 'react-bootstrap/Image'
+import CardImg from 'react-bootstrap'
 import { Row, Col } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <>
-      <Image src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg" fluid={true} rounded={true}/><br /><br />
+      <Card.Img src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg" fluid={true} rounded={true}/><br /><br />
       <Row>
         <Col md={6}>
           The Metropolitan Museum of Art of New York City, colloquially "the Met", is the largest art museum in the Americas. Its permanent collection contains over two million works, 
