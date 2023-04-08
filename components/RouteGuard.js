ae2a039fb0e010d23c060217651ca1d2e9e83347
tBragagnolo <97526243+tBragagnolo@users.noreply.github.com>
2023-04-08 18:38:52 -0400

    update

--- components/RouteGuard.js
+++ components/RouteGuard.js
@@ -4,24 +4,25 @@ import { useState, useEffect } from 'react';
 const PUBLIC_PATHS = ['/login', '/', '/_error'];
 
 export default function RouteGuard(props) {
-  const router = useRouter();
+    const [authorized, setAuthorized] = useState(false);
+    const router = useRouter();
 
-  useEffect(() => {
-    authCheck(router.pathname);
+    useEffect(() => {
+        authCheck(router.pathname);
 
-    router.events.on('routeChangeComplete', authCheck);
+        router.events.on('routeChangeComplete', authCheck);
 
-    return () => {
-      router.events.off('routeChangeComplete', authCheck);
-    };
-  }, []);
+        return () => {
+        router.events.off('routeChangeComplete', authCheck);
+        };
+    }, []);
 
-  function authCheck(url) {
-    const path = url.split('?')[0];
-    if (!PUBLIC_PATHS.includes(path)) {
-      console.log(`trying to request a secure path: ${path}`);
+    function authCheck(url) {
+        const path = url.split('?')[0];
+        if (!PUBLIC_PATHS.includes(path)) {
+        console.log(`trying to request a secure path: ${path}`);
+        }
     }
-  }
 
-  return <>{props.children}</>
+    return <>{props.children}</>
 }
